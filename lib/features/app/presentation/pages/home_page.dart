d7882ee98a5a03d697bec54dbb0a51eaa1d40c55
mamnuu <kioko1908@gmail.com>
2024-05-01 16:07:31 -0500

    Feat: Creación de bottom nav bar

--- lib/features/app/presentation/pages/home_page.dart
+++ lib/features/app/presentation/pages/home_page.dart
@@ -1,22 +1,45 @@
+import 'package:contact_art/features/app/presentation/widgets/bottom_nav_bar.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
-  const HomePage({super.key});
+  const HomePage({Key? key}) : super(key: key);
 
   @override
-  State<HomePage> createState() => _HomePageState();
+  _HomePageState createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
+  int _selectedIndex = 0;
+
+  static const List<Widget> _pages = <Widget>[
+    // Aquí puedes incluir las páginas que desees para cada ítem del menú
+    Text('Inicio'),
+    Text('Buscar'),
+    Text('Favoritos'),
+    Text('Carrito'),
+    Text('Perfil'),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Home Page'),
+        title: Text('ContactArt'),
       ),
-      body: const Center(
-        child: Text('Hello, World!'),
+      body: Center(
+        child: _pages.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavBar(
+        selectedIndex: _selectedIndex,
+        onItemSelected: _onItemTapped,
       ),
     );
   }
 }
+
---
+++ lib/features/app/presentation/widgets/bottom_nav_bar.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class BottomNavBar extends StatelessWidget {
+  final int selectedIndex;
+  final Function(int) onItemSelected;
+
+  const BottomNavBar({
+    Key? key,
+    required this.selectedIndex,
+    required this.onItemSelected,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      items: const <BottomNavigationBarItem>[
+        BottomNavigationBarItem(
+          icon: Icon(Icons.person),
+          label: 'Perfil',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.book),
+          label: 'Información',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.home),
+          label: 'Inicio',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.message),
+          label: 'Chat',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.shopping_cart),
+          label: 'Mi Carrito',
+        ),
+      ],
+      currentIndex: selectedIndex,
+      selectedItemColor: Colors.blue,
+      unselectedItemColor: Color.fromRGBO(75, 7, 87, 1),
+      onTap: onItemSelected,
+    );
+  }
+}
