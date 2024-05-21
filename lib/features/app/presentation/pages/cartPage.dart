32c8c072268ab3d0229632affcf57f8cae9d70c6
mamnuu <kioko1908@gmail.com>
2024-05-21 09:21:28 -0500

    Fix: Se agregan miles al carrito

--- lib/features/app/presentation/pages/cartPage.dart
+++ lib/features/app/presentation/pages/cartPage.dart
@@ -3,6 +3,7 @@ import 'package:contact_art/global/common/toast.dart';
 import 'package:flutter/material.dart';
 import 'package:contact_art/controllers/cartController.dart';
 import 'package:provider/provider.dart';
+import 'package:intl/intl.dart'; 
 
 class CartPage extends StatefulWidget {
   const CartPage({super.key});
@@ -16,6 +17,14 @@ class _CartPageState extends State<CartPage> {
   Widget build(BuildContext context) {
     final cartController = Provider.of<CartController>(context);
     final items = cartController.cartItems;
+
+    final NumberFormat currencyFormat = NumberFormat.currency(
+      locale: 'es_ES',
+      symbol: '\$',
+      customPattern:
+          '\u00A4#,##0.00', 
+    );
+
     return Scaffold(
       appBar: AppBar(
         automaticallyImplyLeading: false,
@@ -24,64 +33,70 @@ class _CartPageState extends State<CartPage> {
       body: ListView.builder(
         itemCount: items.length,
         itemBuilder: (context, index) {
+          final item = items[index];
+          final double price = double.tryParse(item.price.toString()) ?? 0.0;
+          final String itemName = item.name.replaceAll("'", "");
+
           return ListTile(
-            title: Text(items[index].name),
-            subtitle: Text(items[index].price.toString()),
+            title: Text(itemName),
+            subtitle: Text(currencyFormat.format(price)),
             trailing: IconButton(
-                icon: const Icon(Icons.delete),
-                onPressed: () {
-                  showDialog(
-                      context: context,
-                      builder: (BuildContext context) {
-                        return AlertDialog(
-                          title: const Text('Eliminar producto'),
-                          content: const Text(
-                              '¿Estás seguro de que deseas eliminar este producto del carrito?'),
-                          actions: <Widget>[
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: <Widget>[
-                                Expanded(
-                                  child: Padding(
-                                    padding: const EdgeInsets.all(8.0),
-                                    child: ElevatedButton(
-                                      child: const Text(
-                                        'Cancelar',
-                                        style: TextStyle(color: Colors.purple),
-                                      ),
-                                      onPressed: () {
-                                        Navigator.of(context).pop();
-                                      },
-                                    ),
+              icon: const Icon(Icons.delete),
+              onPressed: () {
+                showDialog(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return AlertDialog(
+                      title: const Text('Eliminar producto'),
+                      content: const Text(
+                          '¿Estás seguro de que deseas eliminar este producto del carrito?'),
+                      actions: <Widget>[
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: <Widget>[
+                            Expanded(
+                              child: Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: ElevatedButton(
+                                  child: const Text(
+                                    'Cancelar',
+                                    style: TextStyle(color: Colors.purple),
                                   ),
+                                  onPressed: () {
+                                    Navigator.of(context).pop();
+                                  },
                                 ),
-                                Expanded(
-                                  child: Padding(
-                                    padding: const EdgeInsets.all(8.0),
-                                    child: ElevatedButton(
-                                      style: ElevatedButton.styleFrom(
-                                          backgroundColor: Colors.purple),
-                                      child: Text('Eliminar',
-                                          style:
-                                              TextStyle(color: Colors.white)),
-                                      onPressed: () {
-                                        setState(() {
-                                          items.removeAt(index);
-                                        });
-                                        showToast(
-                                            message:
-                                                "Producto eliminado del carrito");
-                                        Navigator.of(context).pop();
-                                      },
-                                    ),
+                              ),
+                            ),
+                            Expanded(
+                              child: Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: ElevatedButton(
+                                  style: ElevatedButton.styleFrom(
+                                    backgroundColor: Colors.purple,
                                   ),
-                                )
-                              ],
-                            )
+                                  child: const Text('Eliminar',
+                                      style: TextStyle(color: Colors.white)),
+                                  onPressed: () {
+                                    setState(() {
+                                      items.removeAt(index);
+                                    });
+                                    showToast(
+                                        message:
+                                            "Producto eliminado del carrito");
+                                    Navigator.of(context).pop();
+                                  },
+                                ),
+                              ),
+                            ),
                           ],
-                        );
-                      });
-                }),
+                        ),
+                      ],
+                    );
+                  },
+                );
+              },
+            ),
           );
         },
       ),
