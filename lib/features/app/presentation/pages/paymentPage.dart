3e6954979032a75f72aec68eb8575e28814010e1
mamnuu <kioko1908@gmail.com>
2024-06-09 18:54:47 -0500

    Feat: pago con tarjeta

--- lib/features/app/presentation/pages/paymentPage.dart
+++ lib/features/app/presentation/pages/paymentPage.dart
@@ -12,6 +12,9 @@ class PaymentPage extends StatefulWidget {
 
 class _PaymentPageState extends State<PaymentPage> {
   String? _selectedPaymentMethod;
+  final TextEditingController _cardNumberController = TextEditingController();
+  final TextEditingController _expiryDateController = TextEditingController();
+  final TextEditingController _cvvController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -33,8 +36,7 @@ class _PaymentPageState extends State<PaymentPage> {
                 child: const Text('OK'),
                 onPressed: () {
                   Navigator.of(context).pop(); // Cierra el diálogo
-                  Navigator.of(context)
-                      .pop(); // Vuelve a la página anterior (CartPage)
+                  Navigator.of(context).pop(); // Vuelve a la página anterior (CartPage)
                 },
               ),
             ],
@@ -49,8 +51,7 @@ class _PaymentPageState extends State<PaymentPage> {
         builder: (BuildContext context) {
           return AlertDialog(
             title: const Text('Confirmar pedido'),
-            content:
-                const Text('¿Está seguro de que desea realizar el pedido?'),
+            content: const Text('¿Está seguro de que desea realizar el pedido?'),
             actions: <Widget>[
               TextButton(
                 child: const Text('Cancelar'),
@@ -59,11 +60,9 @@ class _PaymentPageState extends State<PaymentPage> {
                 },
               ),
               TextButton(
-                child: const Text('Confirmar',
-                    style: TextStyle(color: Colors.green)),
+                child: const Text('Confirmar', style: TextStyle(color: Colors.green)),
                 onPressed: () {
-                  Navigator.of(context)
-                      .pop(); // Cierra el diálogo de confirmación
+                  Navigator.of(context).pop(); // Cierra el diálogo de confirmación
                   _showSuccessDialog(context); // Muestra el diálogo de éxito
                 },
               ),
@@ -79,8 +78,7 @@ class _PaymentPageState extends State<PaymentPage> {
         builder: (BuildContext context) {
           return AlertDialog(
             title: const Text('Cancelar pedido'),
-            content:
-                const Text('¿Está seguro de que no desea realizar el pedido?'),
+            content: const Text('¿Está seguro de que no desea realizar el pedido?'),
             actions: <Widget>[
               TextButton(
                 child: const Text('No'),
@@ -92,8 +90,7 @@ class _PaymentPageState extends State<PaymentPage> {
                 child: const Text('Sí', style: TextStyle(color: Colors.red)),
                 onPressed: () {
                   Navigator.of(context).pop(); // Cierra el diálogo
-                  Navigator.of(context)
-                      .pop(); // Vuelve a la página anterior (CartPage)
+                  Navigator.of(context).pop(); // Vuelve a la página anterior (CartPage)
                 },
               ),
             ],
@@ -109,73 +106,111 @@ class _PaymentPageState extends State<PaymentPage> {
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                'Total a pagar:',
-                style: Theme.of(context).textTheme.headline5,
-              ),
-              const SizedBox(height: 16),
-              Text(
-                currencyFormat.format(widget.total),
-                style: Theme.of(context)
-                    .textTheme
-                    .headline4
-                    ?.copyWith(color: Colors.green),
-              ),
-              const SizedBox(height: 32),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  RadioListTile<String>(
-                    title: const Text('Pago en efectivo'),
-                    value: 'efectivo',
-                    groupValue: _selectedPaymentMethod,
-                    onChanged: (String? value) {
-                      setState(() {
-                        _selectedPaymentMethod = value;
-                      });
-                    },
-                  ),
-                  RadioListTile<String>(
-                    title: const Text('Pago con tarjeta (datáfono)'),
-                    value: 'tarjeta',
-                    groupValue: _selectedPaymentMethod,
-                    onChanged: (String? value) {
-                      setState(() {
-                        _selectedPaymentMethod = value;
-                      });
-                    },
-                  ),
-                ],
-              ),
-              const SizedBox(height: 32),
-              ElevatedButton(
-                onPressed: _selectedPaymentMethod == null
-                    ? null
-                    : () {
-                        _showOrderConfirmationDialog(context);
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Total a pagar:',
+                  style: Theme.of(context).textTheme.headline5,
+                ),
+                const SizedBox(height: 16),
+                Text(
+                  currencyFormat.format(widget.total),
+                  style: Theme.of(context).textTheme.headline4?.copyWith(color: Colors.green),
+                ),
+                const SizedBox(height: 32),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    RadioListTile<String>(
+                      title: const Text('Pago en efectivo'),
+                      value: 'efectivo',
+                      groupValue: _selectedPaymentMethod,
+                      onChanged: (String? value) {
+                        setState(() {
+                          _selectedPaymentMethod = value;
+                        });
                       },
-                child: const Text('Realizar pedido'),
-              ),
-              const SizedBox(height: 16),
-              ElevatedButton(
-                onPressed: () {
-                  _showCancelConfirmationDialog(context);
-                },
-                style: ElevatedButton.styleFrom(
-                  primary: Colors.red, // Cambia el color del botón a rojo
+                    ),
+                    RadioListTile<String>(
+                      title: const Text('Pago con tarjeta (datáfono)'),
+                      value: 'tarjeta',
+                      groupValue: _selectedPaymentMethod,
+                      onChanged: (String? value) {
+                        setState(() {
+                          _selectedPaymentMethod = value;
+                        });
+                      },
+                    ),
+                  ],
                 ),
-                child: const Text(
-                  'No realizar pedido',
-                  style: TextStyle(color: Colors.white),
+                Visibility(
+                  visible: _selectedPaymentMethod == 'tarjeta',
+                  child: Column(
+                    children: [
+                      TextField(
+                        controller: _cardNumberController,
+                        decoration: const InputDecoration(
+                          labelText: 'Número de tarjeta',
+                        ),
+                        keyboardType: TextInputType.number,
+                      ),
+                      const SizedBox(height: 16),
+                      TextField(
+                        controller: _expiryDateController,
+                        decoration: const InputDecoration(
+                          labelText: 'Fecha de vencimiento (MM/AA)',
+                        ),
+                        keyboardType: TextInputType.datetime,
+                      ),
+                      const SizedBox(height: 16),
+                      TextField(
+                        controller: _cvvController,
+                        decoration: const InputDecoration(
+                          labelText: 'CVV',
+                        ),
+                        keyboardType: TextInputType.number,
+                        obscureText: true,
+                      ),
+                      const SizedBox(height: 32),
+                    ],
+                  ),
                 ),
-              ),
-            ],
+                ElevatedButton(
+                  onPressed: _selectedPaymentMethod == null
+                      ? null
+                      : () {
+                          _showOrderConfirmationDialog(context);
+                        },
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.green, // Cambia el color del botón a verde
+                  ),
+                  child: const Text(
+                    'Realizar pedido',
+                    style: TextStyle(color: Colors.white), // Cambia el color del texto a blanco
+                  ),
+                ),
+                const SizedBox(height: 16),
+                ElevatedButton(
+                  onPressed: () {
+                    _showCancelConfirmationDialog(context);
+                  },
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.red, // Cambia el color del botón a rojo
+                  ),
+                  child: const Text(
+                    'No realizar pedido',
+                    style: TextStyle(color: Colors.white), // Cambia el color del texto a blanco
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
     );
   }
 }
+
+
