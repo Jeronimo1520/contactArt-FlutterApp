434173bd1c64a06fdacd65726710ea6d3e08b846
mamnuu <kioko1908@gmail.com>
2024-06-09 18:43:22 -0500

    Fix: confirmaciones pago

--- lib/features/app/presentation/pages/paymentPage.dart
+++ lib/features/app/presentation/pages/paymentPage.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
-class PaymentPage extends StatelessWidget {
+class PaymentPage extends StatefulWidget {
   final double total;
 
   PaymentPage({required this.total});
+
+  @override
+  _PaymentPageState createState() => _PaymentPageState();
+}
+
+class _PaymentPageState extends State<PaymentPage> {
+  String? _selectedPaymentMethod;
 
   @override
   Widget build(BuildContext context) {
@@ -14,7 +21,7 @@ class PaymentPage extends StatelessWidget {
       customPattern: '\u00A4#,##0.00',
     );
 
-    void _showConfirmationDialog(BuildContext context) {
+    void _showSuccessDialog(BuildContext context) {
       showDialog(
         context: context,
         builder: (BuildContext context) {
@@ -25,8 +32,68 @@ class PaymentPage extends StatelessWidget {
               TextButton(
                 child: const Text('OK'),
                 onPressed: () {
-                  Navigator.of(context).pop(); 
-                  Navigator.of(context).pop(); 
+                  Navigator.of(context).pop(); // Cierra el diálogo
+                  Navigator.of(context)
+                      .pop(); // Vuelve a la página anterior (CartPage)
+                },
+              ),
+            ],
+          );
+        },
+      );
+    }
+
+    void _showOrderConfirmationDialog(BuildContext context) {
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: const Text('Confirmar pedido'),
+            content:
+                const Text('¿Está seguro de que desea realizar el pedido?'),
+            actions: <Widget>[
+              TextButton(
+                child: const Text('Cancelar'),
+                onPressed: () {
+                  Navigator.of(context).pop(); // Cierra el diálogo
+                },
+              ),
+              TextButton(
+                child: const Text('Confirmar',
+                    style: TextStyle(color: Colors.green)),
+                onPressed: () {
+                  Navigator.of(context)
+                      .pop(); // Cierra el diálogo de confirmación
+                  _showSuccessDialog(context); // Muestra el diálogo de éxito
+                },
+              ),
+            ],
+          );
+        },
+      );
+    }
+
+    void _showCancelConfirmationDialog(BuildContext context) {
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: const Text('Cancelar pedido'),
+            content:
+                const Text('¿Está seguro de que no desea realizar el pedido?'),
+            actions: <Widget>[
+              TextButton(
+                child: const Text('No'),
+                onPressed: () {
+                  Navigator.of(context).pop(); // Cierra el diálogo
+                },
+              ),
+              TextButton(
+                child: const Text('Sí', style: TextStyle(color: Colors.red)),
+                onPressed: () {
+                  Navigator.of(context).pop(); // Cierra el diálogo
+                  Navigator.of(context)
+                      .pop(); // Vuelve a la página anterior (CartPage)
                 },
               ),
             ],
@@ -41,26 +108,72 @@ class PaymentPage extends StatelessWidget {
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              'Total a pagar:',
-              style: Theme.of(context).textTheme.headline5,
-            ),
-            const SizedBox(height: 16),
-            Text(
-              currencyFormat.format(total),
-              style: Theme.of(context).textTheme.headline4?.copyWith(color: Colors.green),
-            ),
-            const SizedBox(height: 32),
-            ElevatedButton(
-              onPressed: () {
-                _showConfirmationDialog(context);
-              },
-              child: const Text('Realizar pedido con pago en efectivo'),
-            ),
-          ],
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Total a pagar:',
+                style: Theme.of(context).textTheme.headline5,
+              ),
+              const SizedBox(height: 16),
+              Text(
+                currencyFormat.format(widget.total),
+                style: Theme.of(context)
+                    .textTheme
+                    .headline4
+                    ?.copyWith(color: Colors.green),
+              ),
+              const SizedBox(height: 32),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  RadioListTile<String>(
+                    title: const Text('Pago en efectivo'),
+                    value: 'efectivo',
+                    groupValue: _selectedPaymentMethod,
+                    onChanged: (String? value) {
+                      setState(() {
+                        _selectedPaymentMethod = value;
+                      });
+                    },
+                  ),
+                  RadioListTile<String>(
+                    title: const Text('Pago con tarjeta (datáfono)'),
+                    value: 'tarjeta',
+                    groupValue: _selectedPaymentMethod,
+                    onChanged: (String? value) {
+                      setState(() {
+                        _selectedPaymentMethod = value;
+                      });
+                    },
+                  ),
+                ],
+              ),
+              const SizedBox(height: 32),
+              ElevatedButton(
+                onPressed: _selectedPaymentMethod == null
+                    ? null
+                    : () {
+                        _showOrderConfirmationDialog(context);
+                      },
+                child: const Text('Realizar pedido'),
+              ),
+              const SizedBox(height: 16),
+              ElevatedButton(
+                onPressed: () {
+                  _showCancelConfirmationDialog(context);
+                },
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.red, // Cambia el color del botón a rojo
+                ),
+                child: const Text(
+                  'No realizar pedido',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
