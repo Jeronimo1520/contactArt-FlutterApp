911c4ae862e18847b32fd859aa95a0f2a44c892a
ddragnell <andressalbecerra@gmail.com>
2024-06-10 13:52:49 -0500

    Fix: Carrito añadido a Firebase

--- lib/features/app/presentation/pages/productDetails.dart
+++ lib/features/app/presentation/pages/productDetails.dart
@@ -3,6 +3,7 @@ import 'package:contact_art/controllers/UserController.dart';
 import 'package:contact_art/controllers/cartController.dart';
 import 'package:contact_art/features/app/presentation/pages/chatPage.dart';
 import 'package:contact_art/global/common/toast.dart';
+import 'package:contact_art/models/Cart.dart';
 import 'package:contact_art/models/User.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -90,7 +91,7 @@ class _DetailPageState extends State<DetailPage> {
               ),
             ),
             Spacer(),
-             Row(
+            Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 const Text('Chatea con el vendedor'),
@@ -164,12 +165,7 @@ class _DetailPageState extends State<DetailPage> {
                             ),
                           ),
                           onPressed: () async {
-                            bool result = await cartController.addToCart(
-                                "'${widget.product['name']}'",
-                                '${widget.product['price']}',
-                                '${widget.product['img']}',
-                                1);
-
+                            bool result = await addToCart();
                             if (result) {
                               showToast(
                                   message: "Producto agregado al carrito");
@@ -243,5 +239,19 @@ class _DetailPageState extends State<DetailPage> {
         ),
       ),
     );
+  }
+
+  Future<bool> addToCart() async {
+    String userId = widget.userId;
+    final cartController = Provider.of<CartController>(context, listen: false);
+    Cart cart = Cart(
+      name: widget.product['name'],
+      price: widget.product['price'],
+      img: widget.product['img'],
+      quantity: 1,
+      userId: userId,
+    );
+    String result = await cartController.addToCart(cart);
+    return result != "";
   }
 }
